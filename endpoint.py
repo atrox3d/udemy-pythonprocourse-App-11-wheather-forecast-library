e61ea4021f0c511132ca96881e59812161cb92a3
atrox3d <atrox3d@gmail.com>
2022-09-21 05:54:52 +0200

    update test

--- endpoint.py
+++ endpoint.py
@@ -39,7 +39,17 @@ class OpenWeatherMapEndPoint(EndPoint):
 
 
 if __name__ == '__main__':
-    owmep = OpenWeatherMapEndPoint('https://base.url', 'https://docs.url')
-    print(owmep.base_url)
-    print(owmep.get_url(city='turin', country_code='it', lang='it', test='CIAO'))
-    print(owmep.params)
+    current_weather = OpenWeatherMapEndPoint(
+        base_url='https://api.openweathermap.org/data/2.5/weather',
+        docs='https://openweathermap.org/current',
+    )
+
+    forecast = OpenWeatherMapEndPoint(
+        base_url='https://api.openweathermap.org/data/2.5/forecast',
+        docs='https://openweathermap.org/forecast5',
+    )
+
+    print(current_weather.get_url('turin', country_code='it', units='metric'))
+    print(current_weather.params)
+    print(forecast.get_url('turin', country_code='it', units='metric'))
+    print(forecast.params)
--- sunnyday.py
+++ sunnyday.py
@@ -1,15 +1,2 @@
 from endpoint import OpenWeatherMapEndPoint
 
-current_weather = OpenWeatherMapEndPoint(
-    base_url='https://api.openweathermap.org/data/2.5/weather',
-    docs='https://openweathermap.org/current',
-)
-
-forecast = OpenWeatherMapEndPoint(
-    base_url='https://api.openweathermap.org/data/2.5/forecast',
-    docs='https://openweathermap.org/forecast5',
-)
-
-
-print(current_weather.get_url('turin', country_code='it', units='metric'))
-print(forecast.get_url('turin', country_code='it', units='metric'))
