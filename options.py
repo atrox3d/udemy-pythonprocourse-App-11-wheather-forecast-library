fb3f6649abd54956b3cd956a3a451c593085b17c
atrox3d <atrox3d@gmail.com>
2022-09-26 09:05:04 +0200

    parse_weather_opts nested comprehension

--- options.py
+++ options.py
@@ -6,48 +6,59 @@ class WeatherOptions:
 
     NAME = __qualname__
     
-    WEATHER_OPTS = [
-        '--city',
-        '--country_code',
-        '--lat=',
-        '--lon=',
-        '--lang=',
+    WEATHER_OPTS_NAMES = [
+        'city',
+        'country_code',
+        'lat',
+        'lon',
+        'lang',
     ]
 
-    NEXT_OPTS = [
-        '--hours',
-        '--simplified',
-        '--from_cache'
+    NEXT_OPTS_NAMES = [
+        'hours',
+    ]
+
+    NEXT_OPTS_FLAGS = [
+        'simplified',
+        'from_cache'
     ]
 
     def __init__(self, params=sys.argv):
         self.params = params
+        self.WEATHER_OPTS = []
+        self.NEXT_OPTS = []
         self.opts, self.args = self.getopts(self.params)
         print(self.NAME, ' | ', 'self.params | ', self.params)
         print(self.NAME, ' | ', 'self.opts   | ', self.opts)
         print(self.NAME, ' | ', 'self.args   | ', self.args)
 
+    def build_opts(self):
+        self.WEATHER_OPTS = [f'{opt}=' for opt in self.WEATHER_OPTS_NAMES]
+        print(self.NAME, ' | ', 'self.WEATHER_OPTS | ', self.WEATHER_OPTS)
+
+        self.NEXT_OPTS = [f'{opt}=' for opt in self.NEXT_OPTS_NAMES]
+        self.NEXT_OPTS += self.NEXT_OPTS_FLAGS
+        print(self.NAME, ' | ', 'self.NEXT_OPTS | ', self.NEXT_OPTS)
+
+        return self.WEATHER_OPTS + self.NEXT_OPTS
+
     def getopts(self, params):
         return getopt.getopt(
             params[1:],
-            # "c:C:l:h:",
             "",
-            [
-                'city=',
-                'country_code=',
-                'lat=',
-                'lon=',
-                'lang=',
-                'hours=',
-                'simplified',
-                'from_cache'
-            ]
+            self.build_opts()
         )
 
     def parse_weather_opts(self):
-        weather_opts = {t[0].replace('--', ''): t[1] for t in self.opts if t[0].startswith('--') if t[0] in self.WEATHER_OPTS}
+        weather_opts = {
+            item[0]: item[1] for item in                                        # build dict item
+            [(opt[0].replace('--', ''), opt[1]) for opt in self.opts]           # nested list comprehension, remove -- from options in tuples
+            if item[0] in self.WEATHER_OPTS_NAMES                               # filter only weather options
+        }
+
         print(self.NAME, ' | ', 'weather_opts | ', weather_opts)
         print(self.NAME, ' | ', 'WEATHER_OPTS | ', self.WEATHER_OPTS)
+        exit()
         return weather_opts
 
     def parse_next_opts(self):
